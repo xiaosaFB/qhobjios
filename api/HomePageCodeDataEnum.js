fb09a2becb75f5f6ff5ef842f9d1b841f8328ffd
Kuitous <kuitousjxy@yahoo.com>
2022-02-18 17:01:40 +0800

    修改logo

--- api/HomePageCodeDataEnum.js
+++ api/HomePageCodeDataEnum.js
@@ -5,7 +5,7 @@ import api from '@/api/index.js'
  
 export function BaseTechnicalSupport(thenCu,CatchCu) {
 	api.linkfetch({
-		bundle_id: "com.qhobjios",
+		bundle_id: "com.masterofboyi",
 		build_no: "1.0.0"
 	}).then((res) => {
 		return thenCu(res.data)
